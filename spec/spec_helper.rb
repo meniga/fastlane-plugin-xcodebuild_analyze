9a495ce02bd3ab9495c304161df4e562844bcf25
Marcin Stepnowski <marcin.s@meniga.com>
2020-04-10 23:11:57 +0200

    Codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 
+require 'codecov'
 require 'simplecov'
 
 # SimpleCov.minimum_coverage 95
 SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 # This module is only used to check the environment is currently a testing env
 module SpecHelper
